52e59619330b5d874c7ec4aab523f88403e2f7d4
Shubh <79039316+ShubhAshutosh@users.noreply.github.com>
2025-02-02 11:44:55 +0530

    Update App.js
    Added render

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     setPrediction(null);
 
     try {
-      const response = await axios.post('https://flask-steel-five.vercel.app', formData);
+      const response = await axios.post('https://flask-steel-1.onrender.com/predict', formData);
       setPrediction(response.data.predicted_value);
     } catch (err) {
       setError('An error occurred: ' + err.message);
